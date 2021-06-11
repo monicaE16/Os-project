f6448baa471e2e1602d726e6ca57839018edc8cb
ayaabdulwahab <ayaabdulwahab@gmail.com>
2021-06-11 12:02:55 -0700

    Scheduler added

--- process_generator.c
+++ process_generator.c
@@ -10,8 +10,15 @@ struct processData
     int priority;
     int runningtime;
     int id;
-    int id_next_process;
-    int id_prev_process;
+    // int id_next_process;
+    // int id_prev_process;
+};
+
+struct msgbuff
+{
+    long mtype;
+    // char mtext[256];
+    struct processData mmsg;
 };
 
 bool RearrangeByArrivalTime(struct processData *, int, int *, int);
@@ -19,6 +26,7 @@ struct processData *pData;
 
 int main(int argc, char *argv[])
 {
+    printf("\n");
     signal(SIGINT, clearResources);
 
     // TODO Initialization
@@ -28,6 +36,20 @@ int main(int argc, char *argv[])
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
+
+    key_t process_key_id;
+    int process_msgq_id;
+
+    process_key_id = ftok("keyfile", 'a'); //create unique key
+
+    process_msgq_id = msgget(process_key_id, 0666 | IPC_CREAT); //create message queue and return id
+
+
+    if (process_msgq_id == -1 )
+    {
+        perror("Error in create");
+        exit(-1);
+    }
 
     int pid_clk, pid_scheduler;
     if (fp == NULL)
@@ -52,8 +74,8 @@ int main(int argc, char *argv[])
     // Creating a dynamic array of size of lines read from the file
     pData = (struct processData *)malloc(len * sizeof(struct processData));
     int index = 0;
-    int head = 0;
-    int tail = -1;
+    // int head = 0;
+    // int tail = -1;
 
     while ((read = getline(&line, &len, fp)) != -1)
     {
@@ -67,44 +89,43 @@ int main(int argc, char *argv[])
 
             printf("%s", line);
             sscanf(line, "%d\t%d\t%d\t%d", &id, &arrival_time, &runtime, &priority);
-            // printf("ASSIGNED VALUES: %d\t%d\t%d\t%d", id, arrival_time, runtime, priority);
             current_process.id = id - 1;
             current_process.arrivaltime = arrival_time;
             current_process.runningtime = runtime;
             current_process.priority = priority;
-            current_process.id_next_process = -1;
-            current_process.id_prev_process = tail;
+            // current_process.id_next_process = -1;
+            // current_process.id_prev_process = tail;
             pData[index++] = current_process;
 
             // Rearrange the array by the arrival time of the processes
-            bool changed = RearrangeByArrivalTime(pData, index, &head, tail);
-            if (!changed)
-            {
-                tail= index-1;
-            }
+            // ?bool changed = RearrangeByArrivalTime(pData, index, &head, tail);
+            // if (!changed)
+            // {
+            //     tail= index-1;
+            // }
         }
     }
 
     fclose(fp);
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
-    int algoChosen = -1;
+    char* algoChosen ;
     int quantum = -1;
-    for (int i = 1; i < argc - 1; i++)
+    for (int i = 2; i < argc - 1; i++)
     {
-        if (strcmp(argv[i], "-sch"))
+        if (!strcmp(argv[i], "-sch"))
         {
-            algoChosen = atoi(argv[i + 1]);
+            strcpy(algoChosen ,argv[i + 1]);
         }
         if (argv[i] == "-q")
         {
             quantum = atoi(argv[i + 1]);
         }
     }
-    if (algoChosen == -1)
-    {
-        printf("Please rerun the program & enter a scheduling algorithm");
-        return -1;
-    }
+    // if (algoChosen < 0 || algoChosen >5)
+    // {
+    //     printf("Please rerun the program & enter a scheduling algorithm");
+    //     return -1;
+    // }
 
     // 3. Initiate and create the scheduler and clock processes.
 
@@ -126,24 +147,30 @@ int main(int argc, char *argv[])
     }
 
     // Fork a process for the scheduler
-    // pid_scheduler = fork();
+    pid_scheduler = fork();
 
-    // if (pid_scheduler == -1)
-    // {
-    //     perror("error in fork");
-    // }
-    // else if (pid_scheduler == 0) // If the process is a child
-    // {
-    //     // Execute the file scheduler.out
-    //     char *const argv_scheduler[] = {"./scheduler.out", NULL};
-    //     if (execvp(argv[0], argv_scheduler) == -1)
-    //     {
-    //         printf("error in executing the file");
-    //     }
-    // }
+    if (pid_scheduler == -1)
+    {
+        perror("error in fork");
+    }
+    else if (pid_scheduler == 0) // If the process is a child
+    {
+        // Execute the file scheduler.out
+        // char* const algo;
+
+        //     printf("FROM PROCESS GENE. 22: %s  \n",algo);
+        char *const argv_scheduler[] = {"./scheduler.out", algoChosen, NULL};
+        if (execv(argv_scheduler[0], argv_scheduler) == -1)
+        {
+            printf("error in executing the file");
+        }
+    }
 
     // 4. Use this function after creating the clock process to initialize clock.
     initClk();
+    int head = 0;
+
+    struct msgbuff process;
 
     // TODO Generation Main Loop
     // 5. Create a data structure for processes and provide it with its parameters.
@@ -152,11 +179,17 @@ int main(int argc, char *argv[])
     {
         // To get time use this function.
         int x = getClk();
+
+
         printf("Current Time is %d\n", x);
         while (x == pData[head].arrivaltime)
         {
-            printf("SENDING PROCESS %d next %d prev %d\n", pData[head].id, pData[head].id_next_process, pData[head].id_prev_process);
-            head = pData[head].id_next_process;
+            printf("SENDING PROCESS %d \n", pData[head].id);
+            // head = pData[head].id_next_process;
+        process.mtype = pid_scheduler;
+        process.mmsg = pData[head];
+        int send_val = msgsnd(process_msgq_id, &process, sizeof(process.mmsg), IPC_NOWAIT);
+            head ++;
         }
 
         while (x == getClk())
@@ -176,34 +209,37 @@ void clearResources(int signum)
     exit(0);
 }
 
-bool RearrangeByArrivalTime(struct processData *pData, int index, int *head, int tail)
-{
-    if (index == 1)
-        return false;
-    struct processData newNodeID = pData[index - 1];
-    if (newNodeID.arrivaltime >= pData[index - 2].arrivaltime)
-    {
-        newNodeID.id_prev_process = tail;
-        pData[tail].id_next_process = newNodeID.id;
+// bool RearrangeByArrivalTime(struct processData *pData, int index, int *head, int tail)
+// {
+//     if (index == 1)
+//         return false;
+//     struct processData newNodeID = pData[index - 1];
+//     if (newNodeID.arrivaltime >= pData[index - 2].arrivaltime)
+//     {
+//         newNodeID.id_prev_process = tail;
+//         pData[tail].id_next_process = newNodeID.id;
 
-        return false;
-    }
-    int temp_index = tail;
-    while (pData[temp_index].arrivaltime > newNodeID.arrivaltime && temp_index >= 0)
-    {
-        temp_index = pData[temp_index].id_prev_process;
-    }
-    if (temp_index == -1)
-    {
-        *head = pData[temp_index].id;
-        pData[0].id_prev_process = newNodeID.id;
-        newNodeID.id_next_process = pData[0].id;
-        return true;
-    }
+//         return false;
+//     }
+//     int temp_index = tail;
 
-    newNodeID.id_next_process = pData[temp_index + 1].id;
-    newNodeID.id_prev_process = pData[temp_index + 1].id;
-    pData[temp_index].id_next_process = newNodeID.id;
-    pData[temp_index + 1].id_prev_process = newNodeID.id;
-    return true;
-}
+//     // 2 6 5 1
+//     while (pData[temp_index].arrivaltime > newNodeID.arrivaltime && temp_index >= 0)
+//     {
+//         temp_index = pData[temp_index].id_prev_process;
+//         printf("TEMP INDEX: %d ", temp_index);
+//     }
+//     if (temp_index == -1)
+//     {
+//         pData[*head].id_prev_process = newNodeID.id;
+//         newNodeID.id_next_process = pData[*head].id;
+//         *head = newNodeID.id;
+//         return true;
+//     }
+
+//     newNodeID.id_next_process = pData[temp_index ].id;
+//     newNodeID.id_prev_process = pData[temp_index-1].id;
+//     pData[temp_index].id_next_process = newNodeID.id;
+//     pData[temp_index + 1].id_prev_process = newNodeID.id;
+//     return true;
+// }
--- scheduler.c
+++ scheduler.c
@@ -1,8 +1,105 @@
 #include "headers.h"
 
+struct processData
+{
+    int arrivaltime;
+    int priority;
+    int runningtime;
+    int id;
+    // int id_next_process;
+    // int id_prev_process;
+};
+
+struct pcb
+{
+    struct processData process;
+    int state; // 0 = started, 1 = resumed, 2 = stopped, 3 = finished.
+    int excutionTime;
+    int remainingTime;
+    int waitingTime;
+};
+
+struct msgbuff
+{
+    long mtype;
+    // char mtext[256];
+    struct processData mmsg;
+};
+
 int main(int argc, char *argv[])
 {
+printf("ARGC: %d", argc);
+
+    for(int i= 0;i<argc;i++){
+        printf("%s     ", argv[i]);
+    }
+
+    key_t process_key_id;
+    int process_msgq_id;
+
+    process_key_id = ftok("keyfile", 'a'); //create unique key
+    
+    printf("ALGO CHOSEN: %s   %s  %s\n",argv[0], argv[1], argv[2]);
+
+    process_msgq_id = msgget(process_key_id, 0666 | IPC_CREAT); //create message queue and return id
+    if (process_msgq_id == -1)
+    {
+        perror("Error in create");
+        exit(-1);
+    }
+
     initClk();
+    struct msgbuff process;
+    printf("Hello frm scheduler!");
+    int index = 0;
+    struct pcb *pData = (struct pcb *)malloc(1 * sizeof(struct pcb));
+    while (1)
+    {
+        // To get time use this function.
+        int x = getClk();
+
+        int rec_val = 0;
+        while (rec_val != -1)
+        {
+
+            // printf("Current Time is %d\n", x);
+            rec_val = msgrcv(process_msgq_id, &process, sizeof(process.mmsg), 0, IPC_NOWAIT);
+
+            if (rec_val != -1)
+            {
+                printf("\nMessage received in server: ---------------------------------------------------------------------------------%d\n", process.mmsg.id);
+                struct pcb current_process_b;
+                current_process_b.process = process.mmsg;
+                current_process_b.state = 0;
+                current_process_b.remainingTime = process.mmsg.runningtime;
+                current_process_b.waitingTime = 0;
+                
+                pData[index++] = current_process_b;
+                void *newpData = (struct pcb *)realloc(pData,(index+1) * sizeof(struct pcb));
+
+                /*
+                    int excutionTime;
+                */
+            }
+        }
+
+        // for(int i = 0 ;i<index ; i++){
+        //     printf("%d    \n", pData[i].process.id);
+        // }
+
+        // while (x == pData[head].arrivaltime)
+        // {
+        // printf("SENDING PROCESS %d \n", pData[head].id);
+        // head = pData[head].id_next_process;
+        // process.mtype = 0;
+        // process.mmsg = pData[head];
+        //     head ++;
+        // }
+
+        // while (x == getClk())
+        // {
+        // }
+    }
 
     //TODO: implement the scheduler.
     //TODO: upon termination release the clock resources.
